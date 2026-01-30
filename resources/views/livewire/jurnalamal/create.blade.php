821e6e2358d6c5e4410f1f1d617b6bdb0f7fd00d
dapatinid <dapatin.id@gmail.com>
2026-01-30 17:18:50 +0700

    Update create.blade.php

--- resources/views/livewire/jurnalamal/create.blade.php
+++ resources/views/livewire/jurnalamal/create.blade.php
@@ -73,10 +73,38 @@
             </div>
 
         </form>
-        <x-slot:footer>
-            <x-button type="submit" form="jurnalamal-create" class="w-full md:w-auto">
-                @lang('Save')
-            </x-button>
-        </x-slot:footer>
+        
+<x-slot:footer>
+    <x-button
+        type="submit"
+        form="jurnalamal-create"
+        class="w-full md:w-auto flex items-center justify-center gap-2"
+        wire:loading.attr="disabled"
+    >
+        {{-- Spinner --}}
+        <svg
+            wire:loading
+            class="animate-spin h-4 w-4 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle class="opacity-25" cx="12" cy="12" r="10"
+                    stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor"
+                  d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+        </svg>
+
+        {{-- Text --}}
+        <span wire:loading.remove>
+            @lang('Save')
+        </span>
+
+        <span wire:loading>
+            @lang('Loading...')
+        </span>
+    </x-button>
+</x-slot:footer>
+
     </x-modal>
 </div>
