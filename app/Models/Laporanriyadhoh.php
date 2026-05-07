07bc2bd840c35830c540bed1d1526d011791f4fe
dapatinid <dapatin.id@gmail.com>
2026-05-07 22:21:37 +0700

    fix skor part 08

--- app/Models/Laporanriyadhoh.php
+++ app/Models/Laporanriyadhoh.php
@@ -63,60 +63,36 @@ class LaporanRiyadhoh extends Model
         'dzikir_pagi', 'dzikir_sore', 'alquran', 'puasa_sunnah',
     ];
 
-    // ----------------------------------------------------------------
-    // Accessor: skor  (integer murni, TIDAK termasuk sedekah)
-    // ----------------------------------------------------------------
 
-    protected function skor(): Attribute
-    {
-        return Attribute::make(
-            get: function () {
-                $skor = 0;
-
-                // Rakaat × 100
-                foreach (self::$rakaatKeys as $key) {
-                    $skor += ((int) ($this->attributes[$key] ?? 0)) * 100;
-                }
-
-                // Ucapan × 1
-                foreach (self::$hitunganKeys as $key) {
-                    $skor += (int) ($this->attributes[$key] ?? 0);
-                }
-
-                // Pilihan string
-                foreach (self::$pilihanKeys as $key) {
-                    $val = strtolower((string) ($this->attributes[$key] ?? ''));
-                    $skor += match ($val) {
-                        'sempurna', 'ya', 'jamaah' => 1000,
-                        'sebagian', 'sendiri'      => 500,
-                        default                    => 0,
-                    };
-                }
-
-                return $skor;
-            },
-        );
+public function getSkorAttribute(): int
+{
+    $skor = 0;
+    foreach (self::$rakaatKeys as $key) {
+        $skor += ((int) ($this->attributes[$key] ?? 0)) * 100;
     }
-
-    // ----------------------------------------------------------------
-    // Accessor: skor_gabung  (string, misal "11.200 + Rp30.000")
-    // ----------------------------------------------------------------
-
-    protected function skorGabung(): Attribute
-    {
-        return Attribute::make(
-            get: function () {
-                $skorMurni = $this->skor;
-                $sedekah   = (int) ($this->attributes['sedekah_subuh'] ?? 0);
-
-                $skorStr = number_format($skorMurni, 0, ',', '.');
-
-                if ($sedekah > 0) {
-                    return $skorStr . ' + Rp' . number_format($sedekah, 0, ',', '.');
-                }
-
-                return $skorStr;
-            },
-        );
+    foreach (self::$hitunganKeys as $key) {
+        $skor += (int) ($this->attributes[$key] ?? 0);
     }
+    foreach (self::$pilihanKeys as $key) {
+        $val = strtolower((string) ($this->attributes[$key] ?? ''));
+        $skor += match ($val) {
+            'sempurna', 'ya', 'jamaah' => 1000,
+            'sebagian', 'sendiri'      => 500,
+            default                    => 0,
+        };
+    }
+    return $skor;
+}
+
+public function getSkorGabungAttribute(): string
+{
+    $skorMurni = $this->getSkorAttribute();
+    $sedekah   = (int) ($this->attributes['sedekah_subuh'] ?? 0);
+    $skorStr   = number_format($skorMurni, 0, ',', '.');
+
+    if ($sedekah > 0) {
+        return $skorStr . ' + Rp' . number_format($sedekah, 0, ',', '.');
+    }
+    return $skorStr;
+}    
 }
