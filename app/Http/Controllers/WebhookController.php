d5bd09ad4e80bca3851b375c45ea0fd4b30905ed
dapatinid <dapatin.id@gmail.com>
2026-06-02 22:33:45 +0700

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,30 +9,29 @@ use Illuminate\Support\Facades\Log;
 class WebhookController extends Controller
 {
     public function handleMayar(Request $request)
-        {
-            $payload = $request->getContent();
-            $data = json_decode($payload, true);
-            
-            if (isset($data['event']) && $data['event'] === 'payment.received') {
-                $transactionId = $data['data']['transactionId'] ?? null;
+    {
+        $payload = $request->getContent();
+        $data = json_decode($payload, true);
 
-                if ($transactionId) {
-                    // CARI BERDASARKAN ID YANG KITA SIMPAN TADI
-                    $payment = Payment::where('transaction_id', $transactionId)->first();
+        if (isset($data['event']) && $data['event'] === 'payment.received') {
+            // Mayar sekarang akan mengembalikan reference_id yang kita kirim tadi
+            $referenceId = $data['data']['reference_id'] ?? null; 
 
-                    if ($payment && $payment->status !== 'success') {
-                        $payment->update(['status' => 'success']);
-                        
-                        // Update Infaq Pasangan
-                        Payment::where('mutation_type', 'infaq_sistem')
-                            ->where('created_at', $payment->created_at)
-                            ->where('atas_nama', $payment->atas_nama)
-                            ->where('paymentable_id', $payment->paymentable_id)
-                            ->update(['status' => 'success']);
-                    }
+            if ($referenceId) {
+                $payment = Payment::find($referenceId);
+
+                if ($payment && $payment->status !== 'success') {
+                    $payment->update(['status' => 'success']);
+                    
+                    // Update Infaq Pasangan
+                    Payment::where('mutation_type', 'infaq_sistem')
+                        ->where('created_at', $payment->created_at)
+                        ->where('atas_nama', $payment->atas_nama)
+                        ->where('paymentable_id', $payment->paymentable_id)
+                        ->update(['status' => 'success']);
                 }
             }
-            
-            return response()->json(['message' => 'Success'], 200);
-        }   
+        }
+        return response()->json(['message' => 'Success'], 200);
+    }
 }
